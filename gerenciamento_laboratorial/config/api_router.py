30bc3040161e7085ca7c50467fd38fd62e51b0f8
lidianacostasouza <lidianacostasouza@gmail.com>
2024-10-26 11:27:43 +0000

    adicionando app atendimento junto com sua funcionalidades

--- gerenciamento_laboratorial/config/api_router.py
+++ gerenciamento_laboratorial/config/api_router.py
@@ -5,6 +5,7 @@ from gerenciamento_laboratorial.paciente.api.views import PacienteViewSet
 from gerenciamento_laboratorial.insumo.api.views import InsumoModelViewSet
 from gerenciamento_laboratorial.exame.api.views import ExameViewSet
 from gerenciamento_laboratorial.funcionario.api.views import FuncionarioViewSet
+from gerenciamento_laboratorial.atendimento.api.views import AtendimentoViewSet
 
 router = DefaultRouter()
 
@@ -13,7 +14,8 @@ router.register("users", UserViewSet, basename="users")
 router.register("etiqueta", EtiquetaModelViewSet, basename="etiqueta")
 router.register("pacientes", PacienteViewSet, basename="pacientes")
 router.register("exame", ExameViewSet, basename="exame")
-router.register("funcionarios", FuncionarioViewSet, basename="funcionarios") 
+router.register("funcionarios", FuncionarioViewSet, basename="funcionarios")
 router.register("insumo", InsumoModelViewSet, basename="insumo")
+router.register("atendimento", AtendimentoViewSet, basename="atendimento")
 
 api_router = router
---
+++ gerenciamento_laboratorial/gerenciamento_laboratorial/atendimento/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Atendimento(models.Model):
+    horario_atendimento = models.DateTimeField(
+        verbose_name="Horário do atendimento")
+    limite_fichas = models.PositiveIntegerField(
+        verbose_name="Limite de fichas disponíveis")
+
+    def __str__(self):
+        return f"Atendimento em {self.horario_atendimento} - fichas disponíveis: {self.limite_fichas}"
